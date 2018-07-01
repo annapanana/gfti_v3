ab4438d167b8aae26c15baea41cb4e6fb7ffc415
Anna Lotko <annaklotko@gmail.com>
2018-06-30 21:33:46 -0600

    postcard message displays on back

--- src/js/components/shared/postcards/Back.js
+++ src/js/components/shared/postcards/Back.js
@@ -5,9 +5,19 @@ export default class Back extends React.Component {
     const {data} = this.props;
     return (
       <div class="postcard-back-wrap">
-        <h3>{data.name}</h3>
-        <p>{data.address.delivery_line_1}</p>
-        <p>{data.address.last_line}</p>
+        <div class="col">
+          <h3>{data.name}</h3>
+          <p>{data.address.delivery_line_1}</p>
+          <p>{data.address.last_line}</p>
+        </div>
+        {
+          data.pc_back  &&
+            <div class="col">
+              <small>{data.pc_back.to},</small>
+              <p>{data.pc_back.message}</p>
+              <small>{data.pc_back.from}</small>
+            </div>
+        }
       </div>
     )
   }
