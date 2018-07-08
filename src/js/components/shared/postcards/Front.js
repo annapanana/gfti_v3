3694202e23daa20e7dbaa0d56fbecf5ea47611ae
Anna Lotko <annaklotko@gmail.com>
2018-07-08 07:46:02 -0600

    image dragging is working

--- src/js/components/shared/postcards/Front.js
+++ src/js/components/shared/postcards/Front.js
@@ -3,6 +3,10 @@ import React from "react";
 export default class Front extends React.Component {
   constructor() {
     super();
+    this.startDrag = this.startDrag.bind(this);
+    this.drag = this.drag.bind(this);
+    this.endDrag = this.endDrag.bind(this);
+
     this.state = {
       image_url: "",
       image_scale: "",
@@ -14,9 +18,54 @@ export default class Front extends React.Component {
       font_family: "",
       font_size: "",
       text_pos: "",
-      greetings_text: ""
+      greetings_text: "",
+      selectedElement: null,
+      offset: 0
     }
   }
+
+  componentDidMount() {
+    const draggable = $("#image")[0];
+    draggable.addEventListener('mousedown', this.startDrag);
+    draggable.addEventListener('mousemove', this.drag);
+    draggable.addEventListener('mouseup', this.endDrag);
+    draggable.addEventListener('mouseleave', this.endDrag);
+  }
+
+  startDrag(e) {
+    const selectedElement = e.target,
+          offset = this.getMousePosition(e, selectedElement);
+    offset.x -= parseFloat(selectedElement.getAttributeNS(null, "x")),
+    offset.y -= parseFloat(selectedElement.getAttributeNS(null, "y"));
+    this.setState({
+      selectedElement:selectedElement,
+      offset: offset
+    })
+  }
+
+  drag(e) {
+    const {selectedElement, offset} = this.state;
+    if (selectedElement) {
+      const coord = this.getMousePosition(e, selectedElement);
+      selectedElement.setAttributeNS(null, "x", coord.x - offset.x);
+      selectedElement.setAttributeNS(null, "y", coord.y - offset.y);
+    }
+  }
+
+  endDrag(e) {
+    this.setState({
+      selectedElement: null,
+      offset: 0})
+  }
+
+  getMousePosition(e, elem) {
+    var CTM = elem.getScreenCTM();
+    return {
+      x: (e.clientX - CTM.e) / CTM.a,
+      y: (e.clientY - CTM.f) / CTM.d
+    };
+  }
+
   render() {
     const {data} = this.props,
           pc_front = data.pc_front;
@@ -24,7 +73,7 @@ export default class Front extends React.Component {
       <div class="postcard-front-wrap side">
         <svg viewBox="0 0 400 300">
           <title>Image Test</title>
-          <image width="400" height="300" transform={`scale(${pc_front.image_scale})`} href={data.bg_img}/>
+          <image id="image" width="400" height="300" x="0" y="0" transform={`scale(${pc_front.image_scale})`} href={data.bg_img}/>
         </svg>
       </div>
     )
